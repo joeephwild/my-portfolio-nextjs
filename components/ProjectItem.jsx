65746764aa3ecb73310621bd433fed5c3666de01
joeephwild <josephomotade0@gmail.com>
2022-06-03 04:14:53 +0000

    initial commits

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Link  from 'next/link'
-import Image from 'next/image'
+import Image from 'next/image' 
 
 const ProjectItem = ({title, backgroundImg, projectUrl}) => {
   return <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
         <Image className="rounded-xl group-hover:opacity-10" src={backgroundImg} alt="" />
-      <div className="hidden group-hover:block absolute top-[30%] left-[20%] translate-x-[20%]">
-          <h3 className="text-2xl text-white tracking-wider text-center">{title}</h3>
-          <p className="pb-4 pt-2 text-white text-center">React js</p>
+      <div className="hidden justify-center items-center space-y-12 group-hover:block absolute md:top-[5px] md:left-[5px] top-[20%] left-[20%] translate-x-[15%]">
+          <h3 className="text-3xl md:text-sm text-white tracking-wider text-center">{title}</h3>
           <Link href={projectUrl}>
-              <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg curdor-pointer">More Info</p>
+              <p className="text-center py-3 mx-auto rounded-lg bg-white text-gray-700 font-bold text-lg curdor-pointer">More Info</p>
           </Link>
       </div>
   </div>;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import ProjectItem from './ProjectItem'
-import propertyImg from '../public/assets/projects/property.jpg'
-import cryptoImg from '../public/assets/projects/crypto.jpg'
+import hulu from '../public/assets/projects/hulu.png'
+import race from '../public/assets/projects/race.png'
 import netflixImg from '../public/assets/projects/netflix.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
+import portfolio from '../public/assets/projects/portfolio.png'
+import squid from '../public/assets/projects/squidgame.png'
+import travel from '../public/assets/projects/travel.png'
+import coffee from '../public/assets/projects/coffee.png'
 
 const Projects = () => {
   return <div className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
           <p className="text-xl tracking-widest uppercase text-[#5651e5]">Projects</p>
           <h2 className="py-4 capitalize">my works</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-      <ProjectItem title='property finder' backgroundImg={propertyImg} projectUrl='./property' />
-       <ProjectItem title='property finder' backgroundImg={cryptoImg} projectUrl='./property' />
-      <ProjectItem title='property finder' backgroundImg={netflixImg} projectUrl='./property' />
-      <ProjectItem title='property finder' backgroundImg={twitchImg} projectUrl='./property' />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+      <ProjectItem title='Hulu clone' backgroundImg={hulu} projectUrl='./property' />
+       <ProjectItem title='Squidgame website' backgroundImg={squid} projectUrl='./property' />
+      <ProjectItem title='Netflix webpage' backgroundImg={netflixImg} projectUrl='./property' />
+      <ProjectItem title='Driver race' backgroundImg={race} projectUrl='./property' />
+      <ProjectItem title='travel webpage' backgroundImg={travel} projectUrl='./property' />
+      <ProjectItem title='coffee webpage' backgroundImg={coffee} projectUrl='./property' />
+      <ProjectItem title='portfolio webpage' backgroundImg={portfolio} projectUrl='./property' />
       </div>
     
     </div>
