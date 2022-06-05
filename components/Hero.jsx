2234c2bfbd0978edfd0d1ada9e88a25dd6179b5c
joeephwild <josephomotade0@gmail.com>
2022-06-05 06:51:56 +0000

    initials

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Image from 'next/image'
-import Wave from '../public/assets/wave-haikei.png'
+import { motion } from "framer-motion";
+import { Fade } from "react-reveal";
 import hero from '../public/assets/hero2.png'
 import { AiOutlineClose,AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaLinkedinIn, FaGithubAlt, } from 'react-icons/fa'
 
 
 const Hero = () => {
-  return <div className="w-full h-full text-center">
-      <Image src={Wave} layout="fill" objectFit="cover" alt="" />
-      <div className="">
-      <Image src={hero} layout="fill" objectFit="cover"  alt="" />
-      </div>
-     <div className="max-w-[1240px] px-2 flex-col mt-48 ml-[50%]  justify-center items-center">
-        <p className="uppercase text-sm tracking-widest text-gray-900">Let Build Something together</p>
-        <h1 className="">Hi, Im <span className="text-[#5651e5]">JOSEPH</span></h1>
+  return <div className="w-full mt-24 h-full text-center"> 
+  <div className="hidden md:block">
+  <Image src={hero} layout="fill" objectFit="cover" alt=""className="mt-12"/>
+  </div>
+     <div className="md:max-w-[1240px] px-2 flex-col text-center mt-48 lg:ml-[50%] xl:ml-[60%] justify-center items-center">
+        <p className="uppercase text-3xl lg:text-sm tracking-widest text-gray-900">Let Build Something together</p>
+        <h1 className="text-2xl lg:text-xl">Hi, Im <span className="text-[#5651e5]">JOSEPH</span></h1>
         <h1 className='py-2 text-gray-700'>A Frontend Web Developer</h1>
         <p>Im a frontend web developer specializing in building ( and ocacasionally designing)
             exceptional digital experiences. Currently, Im focused on designing and building respnsive
